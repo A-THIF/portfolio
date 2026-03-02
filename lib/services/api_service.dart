9d826a815b70ef259c731636b303704d53224341
A-THIF <149256653+A-THIF@users.noreply.github.com>
2026-03-02 20:39:32 +0530

    Update api_service.dart
    the input schema is changed according to the json

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -5,26 +5,25 @@ class ApiService {
   static const String baseUrl = "https://portfolio-backend-bnhn.onrender.com";
 
   static Future<bool> sendContact(String username, String link) async {
-    try {
-      final response = await http.post(
-        Uri.parse('$baseUrl/contact'), // your endpoint
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({
-          'username': username,
-          'link': link,
-        }),
-      );
+  try {
+    final response = await http.post(
+      Uri.parse('$baseUrl/contact'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'name': username,
+        'profile_link': link,
+      }),
+    );
 
-      if (response.statusCode == 200) {
-        print("Contact sent successfully");
-        return true;
-      } else {
-        print("Failed to send contact: ${response.body}");
-        return false;
-      }
-    } catch (e) {
-      print("Error sending contact: $e");
+    if (response.statusCode == 200) {
+      print("Contact sent successfully");
+      return true;
+    } else {
+      print("Failed to send contact: ${response.body}");
       return false;
     }
+  } catch (e) {
+    print("Error sending contact: $e");
+    return false;
   }
 }
