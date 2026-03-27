db10400757735dd116b7441c69e0b466e93a3fc1
A-THIF <149256653+A-THIF@users.noreply.github.com>
2026-03-27 19:57:33 +0530

    make it possible to have sound effect in mobile web

--- lib/widgets/sound_effects.dart
+++ lib/widgets/sound_effects.dart
@@ -9,16 +9,12 @@ class SoundEffects {
 
   static bool _hasPlayedOneUp = false;
 
-  // Dynamically check if the user is on a mobile web browser.
-  // If they switch to "Desktop Mode", this will return false!
   static bool get isMobileWeb =>
       kIsWeb &&
       (defaultTargetPlatform == TargetPlatform.iOS ||
           defaultTargetPlatform == TargetPlatform.android);
 
   static Future<void> playJump() async {
-    if (isMobileWeb) return; // Mute for mobile web
-
     try {
       if (_jumpPlayer.state == PlayerState.playing) {
         await _jumpPlayer.stop();
@@ -31,8 +27,6 @@ class SoundEffects {
   }
 
   static Future<void> playWhoa() async {
-    if (isMobileWeb) return; // Mute for mobile web
-
     try {
       if (_whoaPlayer.state == PlayerState.playing) {
         await _whoaPlayer.stop();
@@ -44,8 +38,6 @@ class SoundEffects {
   }
 
   static Future<void> playCoin() async {
-    if (isMobileWeb) return; // Mute for mobile web
-
     try {
       if (_coinPlayer.state == PlayerState.playing) {
         await _coinPlayer.stop();
@@ -57,8 +49,6 @@ class SoundEffects {
   }
 
   static Future<void> playOneUp() async {
-    if (isMobileWeb) return; // Mute for mobile web
-
     if (!_hasPlayedOneUp) {
       try {
         _hasPlayedOneUp = true;
@@ -72,8 +62,6 @@ class SoundEffects {
   }
 
   static Future<void> playFireball() async {
-    if (isMobileWeb) return; // Mute for mobile web
-
     try {
       final player = AudioPlayer()..setReleaseMode(ReleaseMode.release);
       await player.play(AssetSource('audios/mario-fireball.mp3'));
@@ -83,8 +71,6 @@ class SoundEffects {
   }
 
   static Future<void> playSuperMarioBros() async {
-    if (isMobileWeb) return; // Mute for mobile web
-
     try {
       final player = AudioPlayer()..setReleaseMode(ReleaseMode.release);
       await player.play(AssetSource('audios/super-mario-bros.mp3'));
@@ -94,8 +80,6 @@ class SoundEffects {
   }
 
   static Future<void> playOpening() async {
-    if (isMobileWeb) return; // Mute for mobile web
-
     try {
       await _bgPlayer.setReleaseMode(ReleaseMode.stop);
       await _bgPlayer.setSource(AssetSource('audios/mario-opening.mp3'));
